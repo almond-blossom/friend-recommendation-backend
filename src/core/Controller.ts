8e8779c971513423bc4e0e7020ca9d006f42abed
edunga1 <goonr21@gmail.com>
2019-01-10 03:20:44 +0900

    Update Controller

--- src/core/Controller.ts
+++ src/core/Controller.ts
@@ -1,9 +1,21 @@
-import { Request, Response } from 'express';
-
 interface Controller {
   handle(req: Request, res: Response): Promise<void>;
 }
 
+interface Request {
+  body: { [x: string]: any };
+  params: { [x: string]: any };
+}
+
+interface Response {
+  send: Send;
+  json: Send;
+  status(code: number): Response;
+  sendStatus(code: number): Response;
+}
+
+type Send = (body?: any) => Response;
+
 export {
-  Controller, Response, Request,
+  Controller, Request, Response,
 };
